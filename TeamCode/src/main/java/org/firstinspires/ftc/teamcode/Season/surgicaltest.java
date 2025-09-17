8f4ab6280137503b374cda92723661280e4f3fee
senthilnathan <pranavleaps@gmail.com>
2025-09-16 19:44:24 -0700

    dddddd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/surgicaltest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/surgicaltest.java
@@ -7,39 +7,42 @@ import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 @TeleOp
 public class surgicaltest extends LinearOpMode {
-
     DcMotor activeintake = null;
     //DcMotor launchRight = null;
-    private VoltageSensor myControlHubVoltageSensor;
+    //private VoltageSensor myControlHubVoltageSensor;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        myControlHubVoltageSensor = hardwareMap.get(VoltageSensor.class, "Control Hub");
+       // myControlHubVoltageSensor = hardwareMap.get(VoltageSensor.class, "Control Hub");
 
         activeintake = hardwareMap.get(DcMotor.class, "activeintake");
 
-        activeintake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        activeintake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        activeintake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//
+//        activeintake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
         while (opModeIsActive()) {
-            double presentVoltage;
-            presentVoltage = myControlHubVoltageSensor.getVoltage();
-            telemetry.addData("Voltage: ", presentVoltage);
-
-            double powerValue = 0.25;
-            double offsetVoltage = powerValue * (12.5 / presentVoltage);
-            telemetry.addData("offset Voltage: ", offsetVoltage);
+//            double presentVoltage;
+//            presentVoltage = myControlHubVoltageSensor.getVoltage();
+//            telemetry.addData("Voltage: ", presentVoltage);
+//
+//            double powerValue = -0.25;
+//            double offsetVoltage = powerValue * (12.5 / presentVoltage);
+//            telemetry.addData("offset Voltage: ", offsetVoltage);
 
             if (gamepad1.a) {
-                activeintake.setTargetPosition(50);
-
-                activeintake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
                 activeintake.setPower(-0.25);
+            }
+
+            if (gamepad1.b) {
+                activeintake.setPower(0.25);
+            }
+
+            if (gamepad1.x) {
+                activeintake.setPower(0);
             }
         }
     }

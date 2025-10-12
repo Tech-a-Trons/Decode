383b37cfe557af8dcdd93b7d6f5940a1c2803097
senthilnathan <pranavleaps@gmail.com>
2025-10-11 18:25:26 -0700

    moved some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Prototypes/MoveToDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Prototypes/MoveToDistance.java
@@ -2,10 +2,12 @@ package org.firstinspires.ftc.teamcode.Season.Prototypes;
 
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.Season.Subsystems.DistanceLimelightExtractor;
 
+@TeleOp
 public class MoveToDistance extends LinearOpMode {
     Limelight3A limelight;
     DcMotor fl = null;

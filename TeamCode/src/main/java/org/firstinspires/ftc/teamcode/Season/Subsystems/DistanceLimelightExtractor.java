292a945db02867f7e3ec68e75de83db021066564
senthilnathan <pranavleaps@gmail.com>
2025-10-11 17:53:37 -0700

    trying smth new

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Subsystems/DistanceLimelightExtractor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Subsystems/DistanceLimelightExtractor.java
@@ -46,7 +46,7 @@ public class DistanceLimelightExtractor {
             23,19.5,
             24,30.0
     );
-    private final double LIMELIGHT_ANGLE = 10.0;  // degrees
+    private final double LIMELIGHT_ANGLE = 9.95;  // degrees
 
     public DistanceLimelightExtractor(HardwareMap hardwareMap) {
         limelight = hardwareMap.get(Limelight3A.class, "Limelight");

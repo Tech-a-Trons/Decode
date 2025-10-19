6593b5f154f6c58ea385e6982079328fa8b984ea
senthilnathan <pranavleaps@gmail.com>
2025-10-18 18:12:23 -0700

    Reyansh wanted me to commit smth bc his old computer is kinda slow. We tried to connect it to ADB but it didn't work. So, Reyansh sent me this code on Slack...

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Prototypes/ReyanshCode.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.Season.Prototypes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp(name = "DualShooterOuttake")
+public class ReyanshCode extends LinearOpMode {
+//    double x = 0;
+//    double y = 0;
+
+    DcMotor motor2;
+    DcMotor motor1;
+    DcMotor motor3;
+
+    DcMotor motor4;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        motor1 = hardwareMap.get(DcMotor.class, "motor1");
+        motor2 = hardwareMap.get(DcMotor.class, "motor2");
+        motor3 = hardwareMap.get(DcMotor.class, "motor3");
+        motor4 = hardwareMap.get(DcMotor.class, "motor4");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+
+            if (gamepad1.y) {
+                motor3.setPower(0.8);
+            }
+
+            if (gamepad1.b) {
+                motor2.setPower(-0.8);
+            }
+            if (gamepad1.x) {
+                motor1.setPower(-1);
+            }
+            if (gamepad1.a) {
+                motor4.setPower(-1);
+            }
+            if (gamepad1.dpad_right) {
+                motor1.setPower(0);
+                motor2.setPower(0);
+                motor3.setPower(0);
+                motor4.setPower(0);
+            }
+        }
+
+    }
+}

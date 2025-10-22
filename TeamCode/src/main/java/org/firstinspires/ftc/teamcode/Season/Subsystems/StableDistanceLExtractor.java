1882682e59b9d2dd8e99e435f2f5647216627838
senthilnathan <pranavleaps@gmail.com>
2025-10-21 18:13:49 -0700

    some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Subsystems/StableDistanceLExtractor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Subsystems/StableDistanceLExtractor.java
@@ -46,7 +46,7 @@ public class StableDistanceLExtractor {
             23,19.5,
             24,30.0
     );
-    private final double LIMELIGHT_ANGLE = 20;  // degrees
+    private final double LIMELIGHT_ANGLE = 40;  // degrees
 
     public StableDistanceLExtractor(HardwareMap hardwareMap) {
         limelight = hardwareMap.get(Limelight3A.class, "Limelight");
